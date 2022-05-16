8a8863a8bcf4e7fdb6fdab8d82ccd8e47c5a60dd
jjong981028 <jjong981028@gmail.com>
2022-05-16 11:41:21 +0900

    camera

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,190 +1,75 @@
-const socket = io();
+const socket = io()
 
+
+// // //비디오 파트
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
-const camerasSelect = document.getElementById("cameras");
-const call = document.getElementById("call");
+const cameraSelect = document.getElementById("cameras")
 
-call.hidden = true;
-
+ 
 let myStream;
 let muted = false;
 let cameraOff = false;
-let roomName;
-let myPeerConnection;
-let myDataChannel;
 
-async function getCameras() {
+async function getCameras(){
+  try{
+    const devices = await navigator.mediaDevices.enumerateDevices()//나한테 연결된 장치들 정보 가져오기
+    const cameras = devices.filter(device=> device.kind === "videoinput") //내 카메라 정보 가져오기
+    cameras.forEach(camera =>{
+
+    })
+    // const option = document.createElement("option")//적용이 안되서 주석처리 해놨음 중요한거 x
+    // option.value = camera.deviceId
+    // console.log("option은",option)
+    // option.innerText = camera.label
+    // cameraSelect.appendChild(option)//카메라 정보를 라벨로 보이게 해주기
+     
+  }catch(e){
+    console.log(e)
+  }
+}
+ 
+async function getMedia(){ //카메라 오디오 켜기 
   try {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const cameras = devices.filter((device) => device.kind === "videoinput");
-    const currentCamera = myStream.getVideoTracks()[0];
-    cameras.forEach((camera) => {
-      const option = document.createElement("option");
-      option.value = camera.deviceId;
-      option.innerText = camera.label;
-      if (currentCamera.label === camera.label) {
-        option.selected = true;
-      }
-      camerasSelect.appendChild(option);
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
     });
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-async function getMedia(deviceId) {
-  const initialConstrains = {
-    audio: true,
-    video: { facingMode: "user" },
-  };
-  const cameraConstraints = {
-    audio: true,
-    video: { deviceId: { exact: deviceId } },
-  };
-  try {
-    myStream = await navigator.mediaDevices.getUserMedia(
-      deviceId ? cameraConstraints : initialConstrains
-    );
     myFace.srcObject = myStream;
-    if (!deviceId) {
-      await getCameras();
-    }
+    await getCameras()
   } catch (e) {
     console.log(e);
   }
 }
 
-function handleMuteClick() {
+
+function handleMuteClick(){//버튼 누르면 바꿔주기
+  console.log(myStream.getAudioTracks()) //마이크 정보등 나옴
   myStream
     .getAudioTracks()
-    .forEach((track) => (track.enabled = !track.enabled));
-  if (!muted) {
-    muteBtn.innerText = "Unmute";
-    muted = true;
-  } else {
-    muteBtn.innerText = "Mute";
-    muted = false;
+    .forEach((track)=>(track.enabled=!track.enabled))// => 마이크 버튼 누를 때마다 켜고 끄기
+   
+  if(!muted){
+    muteBtn.innerText = "Unmute"
+    muted = true
+  }else{
+    muteBtn.innerText = "Mute"
+    muted = false
   }
 }
-function handleCameraClick() {
+function handleCameraClick(){
   myStream
     .getVideoTracks()
-    .forEach((track) => (track.enabled = !track.enabled));
-  if (cameraOff) {
-    cameraBtn.innerText = "Turn Camera Off";
-    cameraOff = false;
-  } else {
-    cameraBtn.innerText = "Turn Camera On";
-    cameraOff = true;
+    .forEach((track)=>(track.enabled=!track.enabled)) //
+  if(cameraOff){
+    cameraBtn.innerText = "Turn Camera Off" 
+  }else{
+    cameraBtn.innerText = "Turn Camera On"
   }
 }
 
-async function handleCameraChange() {
-  await getMedia(camerasSelect.value);
-  if (myPeerConnection) {
-    const videoTrack = myStream.getVideoTracks()[0];
-    const videoSender = myPeerConnection
-      .getSenders()
-      .find((sender) => sender.track.kind === "video");
-    videoSender.replaceTrack(videoTrack);
-  }
-}
+getMedia();
 
-muteBtn.addEventListener("click", handleMuteClick);
-cameraBtn.addEventListener("click", handleCameraClick);
-camerasSelect.addEventListener("input", handleCameraChange);
-
-// Welcome Form (join a room)
-
-const welcome = document.getElementById("welcome");
-const welcomeForm = welcome.querySelector("form");
-
-async function initCall() {
-  welcome.hidden = true;
-  call.hidden = false;
-  await getMedia();
-  makeConnection();
-}
-
-async function handleWelcomeSubmit(event) {
-  event.preventDefault();
-  const input = welcomeForm.querySelector("input");
-  await initCall();
-  socket.emit("join_room", input.value);
-  roomName = input.value;
-  input.value = "";
-}
-
-welcomeForm.addEventListener("submit", handleWelcomeSubmit);
-
-// Socket Code
-
-socket.on("welcome", async () => {
-  myDataChannel = myPeerConnection.createDataChannel("chat");
-  myDataChannel.addEventListener("message", (event) => console.log(event.data));
-  console.log("made data channel");
-  const offer = await myPeerConnection.createOffer();
-  myPeerConnection.setLocalDescription(offer);
-  console.log("sent the offer");
-  socket.emit("offer", offer, roomName);
-});
-
-socket.on("offer", async (offer) => {
-  myPeerConnection.addEventListener("datachannel", (event) => {
-    myDataChannel = event.channel;
-    myDataChannel.addEventListener("message", (event) =>
-      console.log(event.data)
-    );
-  });
-  console.log("received the offer");
-  myPeerConnection.setRemoteDescription(offer);
-  const answer = await myPeerConnection.createAnswer();
-  myPeerConnection.setLocalDescription(answer);
-  socket.emit("answer", answer, roomName);
-  console.log("sent the answer");
-});
-
-socket.on("answer", (answer) => {
-  console.log("received the answer");
-  myPeerConnection.setRemoteDescription(answer);
-});
-
-socket.on("ice", (ice) => {
-  console.log("received candidate");
-  myPeerConnection.addIceCandidate(ice);
-});
-
-// RTC Code
-
-function makeConnection() {
-  myPeerConnection = new RTCPeerConnection({
-    iceServers: [
-      {
-        urls: [
-          "stun:stun.l.google.com:19302",
-          "stun:stun1.l.google.com:19302",
-          "stun:stun2.l.google.com:19302",
-          "stun:stun3.l.google.com:19302",
-          "stun:stun4.l.google.com:19302",
-        ],
-      },
-    ],
-  });
-  myPeerConnection.addEventListener("icecandidate", handleIce);
-  myPeerConnection.addEventListener("addstream", handleAddStream);
-  myStream
-    .getTracks()
-    .forEach((track) => myPeerConnection.addTrack(track, myStream));
-}
-
-function handleIce(data) {
-  console.log("sent candidate");
-  socket.emit("ice", data.candidate, roomName);
-}
-
-function handleAddStream(data) {
-  const peerFace = document.getElementById("peerFace");
-  peerFace.srcObject = data.stream;
-}
+muteBtn.addEventListener("click",handleMuteClick)
+cameraBtn.addEventListener("click",handleCameraClick)
